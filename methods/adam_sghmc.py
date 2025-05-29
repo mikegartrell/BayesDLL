Fixed bug in gradient update for SGHMC and Adam-SGHMC.
Fixed bug in gradient update for SGHMC and Adam-SGHMC, so that the gradient update uses the momentum properly.

methods/adam_sghmc.py
@@ -627,8 +627,8 @@ class Model(nn.Module):
                     self.m[pname] = m
                     self.v[pname] = v
                     
-                    # Update gradient using momentum
-                    p.grad = p.grad + v_momentum.clone()
+                    # Set gradient to momentum, for SGD optimizer to use
+                    p.grad = v_momentum.clone()
         
         return loss.item(), out.detach()
     

methods/sghmc.py
@@ -582,8 +582,8 @@ class Model(nn.Module):
                     # Store updated momentum
                     self.momentum_buffer[pname] = v
                     
-                    # Update gradient using momentum
-                    p.grad = p.grad + v.clone()
+                    # Set gradient to momentum, for SGD optimizer to use
+                    p.grad = v.clone()
         
         return loss.item(), out.detach()
 