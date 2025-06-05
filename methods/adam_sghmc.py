Fixed gradient update LR scale issue in SGHMC and Adam-SGHMC.
Fixed a gradient update issue in the SGHMC and Adam-SGHMC implementations, linked to LR scaling of updates caused by the SGD optimizer (issue found by Omar). The model parameters are now updated directly using the momentum, without the SGD optimizer.

methods/adam_sghmc.py
@@ -252,7 +252,7 @@ class Runner:
                     self.Ninflate, self.nd
                 )
 
-                self.optimizer.step()  # update self.net (ie, theta)
+                # self.optimizer.step()  # update self.net (ie, theta)
 
                 # prediction on training
                 pred = out.data.max(dim=1)[1]
@@ -633,9 +633,9 @@ class Model(nn.Module):
                     self.momentum_buffer[pname] = v_momentum
                     self.m[pname] = m
                     self.v[pname] = v
-                    
-                    # Set gradient to momentum, for SGD optimizer to use
-                    p.grad = -v_momentum.clone()
+
+                    # Update parameters using momentum
+                    p.data.add_(v_momentum)
         
         return loss.item(), out.detach()
     

methods/sghmc.py
@@ -240,7 +240,7 @@ class Runner:
                     self.Ninflate, self.nd
                 )
 
-                self.optimizer.step()  # update self.net (ie, theta)
+                # self.optimizer.step()  # update self.net (ie, theta)
 
                 # prediction on training
                 pred = out.data.max(dim=1)[1]
@@ -581,9 +581,9 @@ class Model(nn.Module):
                     
                     # Store updated momentum
                     self.momentum_buffer[pname] = v
-                    
-                    # Set gradient to momentum, for SGD optimizer to use
-                    p.grad = -v.clone()
+
+                    # Update parameters using momentum
+                    p.data.add_(v)
         
         return loss.item(), out.detach()
 